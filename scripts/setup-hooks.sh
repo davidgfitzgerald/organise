a4a3f703f7c35f8836daab65105c6061a0ddc20f
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-03-31 22:12:24 +0100

    Add shellcheck pre-commit

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+which pre-commit
+
+if [ $? -ne 0 ]; then
+    echo "pre-commit is not installed. Please install it using 'brew install pre-commit'"
+    exit 1
+fi
+
 # Create the .git/hooks directory if it doesn't exist
 mkdir -p .git/hooks
 
 # Create symbolic link to our pre-commit hook
 ln -sf ../../scripts/pre-commit .git/hooks/pre-commit
+
+# Install the pre-commit hooks
+pre-commit install
 
 echo "Git hooks have been set up successfully!" 
