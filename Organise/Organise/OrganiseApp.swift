40b4e18bf884b6d2a8b9a0fb7a89fe1e851f8db4
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-05-13 22:59:37 +0100

    Create new swift project

---
+++ Organise/Organise/OrganiseApp.swift
@@ -0,0 +1,32 @@
+//
+//  OrganiseApp.swift
+//  Organise
+//
+//  Created by David Fitzgerald on 13/05/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OrganiseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Organise/OrganiseTests/OrganiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrganiseTests.swift
+//  OrganiseTests
+//
+//  Created by David Fitzgerald on 13/05/2025.
+//
+
+import Testing
+@testable import Organise
+
+struct OrganiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
