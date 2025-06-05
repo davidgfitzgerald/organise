836134803c1054cce4a8dc8f510ed604147c6381
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-06-05 01:03:12 +0100

    Sort the activities logically

--- Organise/Organise/Views/Activities/ActivityDayList.swift
+++ Organise/Organise/Views/Activities/ActivityDayList.swift
@@ -11,12 +11,25 @@ import SwiftData
 
 struct ActivityDayList: View {
     @Query(sort: \Activity.completedAt) private var allActivities: [Activity]
+    
+    private var sortedActivities: [Activity] {
+        let incomplete = allActivities.filter { $0.completedAt == nil }
+        
+        let completed = allActivities.filter { $0.completedAt != nil }
+            .sorted {
+                guard let date1 = $0.completedAt, let date2 = $1.completedAt else { return false }
+                return date1 > date2 // Most recent first
+            }
+        
+        return incomplete + completed
+    }
+
     var body: some View {
         VStack {
             Text("Activities")
                 .font(.title)
             List {
-                ForEach(allActivities) { activity in
+                ForEach(sortedActivities) { activity in
                     ActivityRow(activity: activity)
                         .id(activity.id)
                 }
